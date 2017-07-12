427a68952f3037c39fe79c02bbef4ef3fd917f44
Brian Edwards <brian.edwards@yodle.com>
2017-07-12 15:06:04 -0500

    Update ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject artillery "0.1.0-SNAPSHOT"
+(defproject artillery "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
